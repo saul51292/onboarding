8ba9d38a1dac0c2637a6c0d709385a69cf5adc08
Saul <saul@SaulMacBook.local>
2016-09-01 17:25:07 -0400

    issues

--- Onboarding/CarouselView.swift
+++ Onboarding/CarouselView.swift
@@ -10,12 +10,113 @@ import UIKit
 
 class CarouselView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bottomView: UIView!
+    let placeholderImage = UIImageView(image: UIImage(named: "hook up.png"))
+    let hiPlaceholderImage = UIImageView(image: UIImage(named: "hiBig.png"))
 
+    func addUserSpecificCarouselView(carouselView: UIView) {
+        topView.addSubview(carouselView)
+        carouselView.contentMode = .ScaleAspectFill
+        carouselView.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 100.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0))
+        setLineSpacing(topLabel)
+        addHiPlaceholder()
+    }
+    
+    func addHiPlaceholder() {
+        topView.addSubview(hiPlaceholderImage)
+        hiPlaceholderImage.contentMode = .ScaleAspectFill
+        hiPlaceholderImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: hiPlaceholderImage, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 100.0))
+        self.addConstraint(NSLayoutConstraint(item: hiPlaceholderImage, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: -8.0))
+        self.addConstraint(NSLayoutConstraint(item: hiPlaceholderImage, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 10.0))
+        self.addConstraint(NSLayoutConstraint(item: hiPlaceholderImage, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0))
+        
+    }
+    
+    func addPlaceholder() {
+        self.addSubview(placeholderImage)
+        placeholderImage.contentMode = .ScaleAspectFit
+        placeholderImage.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: placeholderImage, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: placeholderImage, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: placeholderImage, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0))
+        
+    }
+    
+    func addLineCarouselView(carouselView: UIView) {
+        topView.addSubview(carouselView)
+        carouselView.contentMode = .ScaleAspectFill
+        carouselView.layer.allowsEdgeAntialiasing = true
+        carouselView.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0))
+        setLineSpacing(topLabel)
+        
+    }
+    
+    func addCardCarouselView(carouselView: UIView) {
+        self.addSubview(carouselView)
+        carouselView.contentMode = .ScaleAspectFill
+        carouselView.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0))
+        self.addConstraint(NSLayoutConstraint(item: carouselView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0.0))
+        addPlaceholder()
+    }
+    
+    func addWhyButton() {
+        let whyButton = UIButton()
+        whyButton.setTitle("Why?", forState: .Normal)
+        whyButton.setTitleColor(UIColor.greenColor(), forState: .Normal)
+        bottomView.addSubview(whyButton)
+        whyButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addConstraint(NSLayoutConstraint(item: whyButton, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 50.0))
+        self.addConstraint(NSLayoutConstraint(item: whyButton, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 22.0))
+        self.addConstraint(NSLayoutConstraint(item: whyButton, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: topLabel, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 24.0))
+        self.addConstraint(NSLayoutConstraint(item: whyButton, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: topLabel, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0.0))
+        
+        
+    }
+    
+    func unhideAndSetSubLabel(text: String) {
+        nameLabel.hidden = false
+        nameLabel.text = text
+    }
+    
+    func removeHiPlaceholder() {
+        hiPlaceholderImage.removeFromSuperview()
+    }
+    
+    func removePlaceholder() {
+        placeholderImage.removeFromSuperview()
+    }
+    
+    func setLineSpacing(label: UILabel) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 2.5
+        
+        let attrString = NSMutableAttributedString(string: label.text!)
+        attrString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSMakeRange(0, attrString.length))
+        label.attributedText = attrString
+    }
+    
+    func bottomLabelText(text: String) {
+        bottomLabel.text = text
+    }
+    
+    func topLabelText(text: String) {
+        topLabel.text = text
+    }
+    
+    
+    
 }
--- Onboarding/ViewController.swift
+++ Onboarding/ViewController.swift
@@ -1,19 +1,241 @@
-//
-//  ViewController.swift
-//  Onboarding
-//
-//  Created by Saul on 8/30/16.
 //  Copyright © 2016 Saul. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+private let kNumberOfScreens: Int = 5
+private let kMarginForAlpha: CGFloat = 150.0
 
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var frame = CGRectMake(0, 0, 0, 0)
+    
+    // Adding in CarouselViews
+    var carouselViewOne = Carousel1AnimationView()
+    var carouselViewTwo = Carousel2ForwardView()
+    var carouselViewThree = CarouselCardsView()
+    var carouselViewThreeFade = Carousel3View()
+    var carouselViewFour = Carousel4AnimationView()
+    var carouselViewFive = Carousel5AnimationView()
+    
+    // Flags to make sure user is on specific screen
+    var onScreenTwo = false
+    var onScreenThree = false
+    var onScreenFour = false
+    var onScreenFive = false
+    
+    // Stock Images
+    let stockImageView = UIImageView(image: UIImage(named: "stock_1.jpg"))
+    let secondStockImageView = UIImageView(image: UIImage(named: "stock_2.jpg"))
+    let thirdStockImageView = UIImageView(image: UIImage(named: "stock_3.jpg"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set scrollView
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.directionalLockEnabled = true
+        scrollView.delegate = self
+        scrollView.pagingEnabled = true
+        
+        for index in 0...kNumberOfScreens {
+            
+            frame.origin.x = self.view.frame.size.width * CGFloat(index)
+            frame.size = self.view.frame.size
+            
+            if let nib = instanceFromNib() as? CarouselView {
+                self.scrollView.addSubview(nib)
+                nib.frame.origin.x = self.view.frame.size.width * CGFloat(index)
+                switch(index) {
+                case 0:
+                    carouselViewOne.hidden = true
+                    nib.addUserSpecificCarouselView(carouselViewOne)
+                    nib.topLabelText("Welcome to the brand new Hinge.")
+                    nib.unhideAndSetSubLabel("Jean Marie,")
+                case 1:
+                    carouselViewTwo.hidden = true
+                    nib.addLineCarouselView(carouselViewTwo)
+                    addImageView(stockImageView)
+                    nib.topLabelText("We built this for you, and for everyone who's over the games:")
+                case 2:
+                    carouselViewThree.hidden = true
+                    nib.addCardCarouselView(carouselViewThree)
+                    
+                    carouselViewThreeFade.hidden = true
+                    nib.addLineCarouselView(carouselViewThreeFade)
+                    addImageView(secondStockImageView)
+                    nib.topLabelText("If you're ready to be treated like a person, not a playing card.")
+                case 3:
+                    carouselViewFour.hidden = true
+                    nib.addLineCarouselView(carouselViewFour)
+                    addImageView(thirdStockImageView)
 
-        // Do any additional setup after loading the view.
+                    nib.topLabelText("And ready to find what you're looking for.")
+                    nib.bottomLabelText("We got you.")
+                    nib.setLineSpacing(nib.bottomLabel)
+                case 4:
+                    carouselViewFive.hidden = true
+                    nib.addLineCarouselView(carouselViewFive)
+                    nib.topLabelText("Hinge is a $7/mo members-only \nexperience.")
+                    nib.unhideAndSetSubLabel("The Relationship App")
+                    nib.addWhyButton()
+                default:
+                    break
+
+                }
+            }
+        }
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width * CGFloat(kNumberOfScreens), self.view.frame.size.height)
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let screenWidth = UIScreen.mainScreen().bounds.width
+        
+        // makes sure user can't scroll backwards
+        if scrollView.contentOffset.x <= 0.0 {
+            scrollView.setContentOffset(CGPointMake(0.0, 0.0), animated: false)
+        }
+        if onScreenTwo && scrollView.contentOffset.x < screenWidth {
+            scrollView.setContentOffset(CGPointMake(screenWidth, 0.0), animated: false)
+        }
+        if onScreenThree && scrollView.contentOffset.x < screenWidth * 2 {
+            scrollView.setContentOffset(CGPointMake(screenWidth * 2, 0.0), animated: false)
+        }
+        if onScreenFour && scrollView.contentOffset.x < screenWidth * 3 {
+            scrollView.setContentOffset(CGPointMake(screenWidth * 3, 0.0), animated: false)
+        }
+        if onScreenFive {
+            scrollView.setContentOffset(CGPointMake(screenWidth * 4, 0.0), animated: false)
+        }
+        
+        // controls alpha of image when entering
+        switch scrollView.contentOffset.x {
+        case (screenWidth - kMarginForAlpha)...screenWidth:
+            stockImageView.alpha = (1 + (scrollView.contentOffset.x - screenWidth)/kMarginForAlpha)
+        case ((screenWidth * 3) - kMarginForAlpha)...screenWidth * 3:
+            thirdStockImageView.alpha = (1 + (scrollView.contentOffset.x - screenWidth * 3)/kMarginForAlpha)
+        default:
+            break
+        }
+        
+        // controls alpha of image when leaving a screen
+        if onScreenTwo && (scrollView.contentOffset.x >= (screenWidth - kMarginForAlpha)) {
+            stockImageView.alpha = (1 - (scrollView.contentOffset.x - screenWidth)/kMarginForAlpha)
+        }
+        if onScreenThree && (scrollView.contentOffset.x >= (screenWidth * 2) - kMarginForAlpha) {
+            secondStockImageView.alpha = (1 - (scrollView.contentOffset.x - screenWidth * 2)/kMarginForAlpha)
+        }
+        if onScreenFour && (scrollView.contentOffset.x >= (screenWidth * 3) - kMarginForAlpha) {
+            thirdStockImageView.alpha = (1 - (scrollView.contentOffset.x - screenWidth * 3)/kMarginForAlpha)
+        }
+        
+        switch scrollView.contentOffset.x {
+        case screenWidth:
+            if !checkIfOnScreen(Int(screenWidth/screenWidth)) {
+                scrollView.scrollEnabled = false
+                carouselViewTwo.addCarousel2ForwardAnimation({ (_) in
+                    scrollView.scrollEnabled = true
+                    self.onScreenTwo = true
+                })
+            }
+        case screenWidth * 2:
+            self.performSelector(#selector(ViewController.animateImageViewIn), withObject: nil, afterDelay: 7.42)
+            if !checkIfOnScreen(Int((screenWidth * 2)/screenWidth)) {
+                if let carouselView = carouselViewThree.superview as? CarouselView {
+                    carouselView.removePlaceholder()
+                }
+                scrollView.scrollEnabled = false
+                carouselViewThree.addCarousel3CardsAnimation({ (_) in
+                    self.carouselViewThreeFade.hidden = false
+                    
+                    self.carouselViewThreeFade.addCarousel32Animation({ (_) in
+                        scrollView.scrollEnabled = true
+                        self.onScreenThree = true
+                    })
+                })
+            }
+        case screenWidth * 3:
+            if !checkIfOnScreen(Int((screenWidth * 3)/screenWidth)) {
+                scrollView.scrollEnabled = false
+                carouselViewFour.addCarousel4Animation({ (_) in
+                    scrollView.scrollEnabled = true
+                    self.onScreenFour = true
+                })
+            }
+        case screenWidth * 4:
+            if !checkIfOnScreen(Int((screenWidth * 4)/screenWidth)) {
+                scrollView.scrollEnabled = false
+                carouselViewFive.addCarousel5Animation({ (_) in
+                    scrollView.scrollEnabled = true
+                    self.onScreenFive = true
+                })
+            }
+        default:
+            break
+        
+        }
+    }
+
+    func checkIfOnScreen(index: Int) -> Bool {
+        switch(index) {
+        case 1:
+            pageControl.currentPage = 1
+            carouselViewTwo.hidden = false
+            return onScreenTwo
+        case 2:
+            pageControl.currentPage = 2
+            carouselViewThree.hidden = false
+            return onScreenThree
+        case 3:
+            pageControl.currentPage = 3
+            carouselViewFour.hidden = false
+            return onScreenFour
+        case 4:
+            pageControl.currentPage = 4
+            carouselViewFive.hidden = false
+            return onScreenFive
+        default:
+            return false
+        }
+    }
+    
+    func animateImageViewIn() {
+        UIView.animateWithDuration(0.2, animations: {
+            self.secondStockImageView.alpha = 1.0
+        })
+    }
+    
+    func addImageView(stockImageView: UIImageView) {
+        stockImageView.contentMode = .ScaleAspectFill
+        topView.insertSubview(stockImageView, atIndex: 0)
+        stockImageView.alpha = 0.0
+        stockImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraint(NSLayoutConstraint(item: stockImageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: stockImageView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 10.0))
+        self.view.addConstraint(NSLayoutConstraint(item: stockImageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: stockImageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: topView, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10.0))
+    }
+    
+    func instanceFromNib() -> UIView {
+        return UINib(nibName: "CarouselView", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! UIView
+    }
+    
+    func animateHi() {
+        carouselViewOne.hidden = false
+        if let carouselView = carouselViewOne.superview?.superview?.superview as? CarouselView {
+            carouselView.removeHiPlaceholder()
+        }
+        carouselViewOne.addCarousel1Animation()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.performSelector(#selector(ViewController.animateHi), withObject: nil, afterDelay: 1.38)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +243,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
